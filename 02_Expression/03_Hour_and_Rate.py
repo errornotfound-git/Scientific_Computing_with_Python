1ac410103b0032821a4c5f1f6d729a5810245d7a
Thien H <huynhthienan@outlook.com>
2022-06-16 00:07:27 -0400

    Update on 03_Hour_and_Rate
    2nd option for the last code line

--- 02_Expression/03_Hour_and_Rate.py
+++ 02_Expression/03_Hour_and_Rate.py
@@ -2,4 +2,4 @@
 ihr = int(input('Enter Hours: ')) # Input hours will be automatically transform into integer
 irt = float(input('Enter Rate: ')) # Input rate will be automatically transform into float, if not > cannot calculate as input's default is str()
 grp = round((ihr * irt),2) # Round the result to 2 decimal
-print('Pay: ' + str(grp))
+print('Pay: ',grp) # print('Pay: ' + str(grp)) also works
