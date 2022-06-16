66c2d7ca33f3948181a8c4e6df07caa4f7953b11
Thien H <huynhthienan@outlook.com>
2022-06-16 00:01:29 -0400

    Add Excerise 02 and 03 for Chapter 2
    02_Welcome_Message and 03_Hour_and_Rate. Remember that input() always result as a string

---
+++ 02_Expression/03_Hour_and_Rate.py
@@ -0,0 +1,5 @@
+# Exercise 3: Write a program to prompt the user for hours and rate per hour to compute gross pay.
+ihr = int(input('Enter Hours: ')) # Input hours will be automatically transform into integer
+irt = float(input('Enter Rate: ')) # Input rate will be automatically transform into float, if not > cannot calculate as input's default is str()
+grp = round((ihr * irt),2) # Round the result to 2 decimal
+print('Pay: ' + str(grp))
